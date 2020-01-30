3558aee92e2726b160e2642584f9e6ddbe011d67
felixnaredi <naredi@student.chalmers.se>
2020-01-30 10:45:56 +0100

    Actor interface

---
+++ src/main/java/chalmers/dat055/achtung/Actor.java
@@ -0,0 +1,8 @@
+package chalmers.dat055.achtung;
+
+import javafx.scene.Group;
+
+public interface Actor {
+  public void update();
+  public void draw(Group group);
+}
--- src/main/java/chalmers/dat055/achtung/Curve.java
+++ src/main/java/chalmers/dat055/achtung/Curve.java
@@ -1,12 +1,8 @@
 package chalmers.dat055.achtung;
 
-import javafx.scene.Group;
-
-interface Curve {
+interface Curve extends Actor {
   public void addPitch(double rad);
   public void setPosition(double x, double y);
   public void setSpeed(double speed);
   public void setStrokeWidth(int width);
-  public void update();
-  public void draw(Group group);
 }
