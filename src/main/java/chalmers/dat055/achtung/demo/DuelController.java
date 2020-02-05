d5491e41103c60d860e8c3f4d485f6eb6b9d78cd
felixnaredi <naredi@student.chalmers.se>
2020-02-05 16:51:47 +0100

    A bit more dynamic menu behavior

--- src/main/java/chalmers/dat055/achtung/demo/DuelController.java
+++ src/main/java/chalmers/dat055/achtung/demo/DuelController.java
@@ -3,11 +3,26 @@ package chalmers.dat055.achtung.demo;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class DuelController implements Initializable {
+  @FXML private Label playButton;
+  @FXML private Label pauseButton;
+
+  private boolean mGameIsRunning;
+
+  private void setGameIsRunning(boolean value) {
+    mGameIsRunning = value;
+    playButton.setText(value ? "Reset" : "Start");
+    pauseButton.setDisable(!value);
+  }
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    System.out.println("hello!");
+    setGameIsRunning(false);
+    
+    playButton.setOnMousePressed((e) -> setGameIsRunning(!mGameIsRunning));
   }
 }
